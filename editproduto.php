8bda86738be1269b3a743fbfb666c00a0ca7767e
carolfps <37373110+carolfps@users.noreply.github.com>
2020-02-11 17:12:04 -0300

    correcao e melhoria do php

--- editproduto.php
+++ editproduto.php
@@ -1,66 +1,64 @@
 <?php
+
 session_start();
+
 //se não tiver logado redireciona para a pagina de login
 if(!isset($_SESSION["email"])){
     header("Location: login.php");
 }
+
 //verifica se o id foi passado por get e existe o json com os dados dos produtos
 if ($_GET["id"] and file_exists('cadastprodutos.json')) {
-    $produtoscadast = file_get_contents("cadastprodutos.json");
-    $produtoscadast = json_decode($produtoscadast, true);
-    $posicao = array_search($_GET["id"], array_column($produtoscadast, 'id'));
+
+    $dadosatuais = file_get_contents("cadastprodutos.json");
+    $dadosatuais = json_decode($dadosatuais, true);
+    $posicao = array_search($_GET["id"], array_column($dadosatuais, 'id'));
+
 }
 
 if ($_POST) {
-    //se nada tiver sido colocado na descricao do produto, coloca um array vazio
-    if (!isset($_POST["descricao-produto"])) {
-        $_POST["descricao-produto"] = "";
-    }
+
+    //inserindo os dados editados do produto
     $produtoEdit = array(
-        "nome" => $_POST["nome-produto"],
-        "preco" => $_POST["preco-produto"],
-        "descricao" => $_POST["descricao-produto"],
-        "id" => $_GET["id"]
+        "nome" => $_POST["nome"],
+        "preco" => $_POST["preco"],
+        "descricao" => $_POST["descricao"],
+        "id" => $_GET["id"],
+        "Enderecofoto" => $dadosatuais[$posicao]["Enderecofoto"]
     );
 
     if (file_exists('cadastprodutos.json')) {
 
-        $dadosatuais = file_get_contents("cadastprodutos.json");
-        $temporario = json_decode($dadosatuais, true);
-        
-        if (isset($_FILES)) {
-            $ext = pathinfo($_FILES["fotoProd"]["name"], PATHINFO_EXTENSION);
+        //se uma nova foto for inserida
+        if ($_FILES["fotoProd"]["error"]==0) {
+
+            //renomeando o arquivo e salvando ele na pasta uploads
+            $ext = strtolower(pathinfo($_FILES["fotoProd"]["name"], PATHINFO_EXTENSION));
             $nome = "Produto".$produtoEdit["id"].".".$ext;
             $arquivo = $_FILES["fotoProd"]["tmp_name"];
             $caminho = "uploads\\" . $nome;
             $produtoEdit["Enderecofoto"] = $caminho;
-            $uploadOk = 1;
-            $imageFileType = strtolower(pathinfo($caminho, PATHINFO_EXTENSION));
             
             //permite apenas determinadas extensões de arquivo
-            if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif") {
-                $uploadOk = 0;
-            }
-            //checa se $uploadOk é zero, ou seja, se teve algum erro
-            if ($uploadOk == 0) {
-                echo "Desculpa, não foi possível subir o seu arquivo.";
-                
-                //se tudo estiver OK tenta subir o arquivo
+            if ($ext != "jpg" && $ext != "png" && $ext != "jpeg" && $ext != "gif") {
+                echo "Desculpa, não foi possível subir o seu arquivo."; 
             } else {
+                //se o arquivo tiver a extensão desejada
                 $movendo = move_uploaded_file($arquivo, $caminho);
             }
         }
+        
+        //atualizando os dados do produto
+        $dadosatuais[$posicao] = $produtoEdit;
 
-        if (file_exists('cadastprodutos.json')) {
-            $dadosatuais = file_get_contents("cadastprodutos.json");
-            $temporario = json_decode($dadosatuais, true);
-            $posicao = array_search($_GET["id"], array_column($temporario, 'id')); 
-            $temporario[$posicao] = $produtoEdit;
-            $jsonData = json_encode($temporario);
-            file_put_contents("cadastprodutos.json", $jsonData);
-            header('Location: indexprodutos.php');
-            exit;
-        }
+        //inserindo os dados no json
+        $jsonData = json_encode($dadosatuais);
+        file_put_contents("cadastprodutos.json", $jsonData);
+
+        //após enviar os dados para o json redireciona para a página index
+        header('Location: indexprodutos.php');
+        exit;
+        
     }
 }
 ?>
@@ -81,27 +79,27 @@ if ($_POST) {
             <div class="my-3">
                 <div class="form-row">
                     <div class="col-md-6">
-                        <label for="nome-produto">Nome</label>
-                        <input type="text" class="form-control" name="nome-produto" id="nome-produto" value="<?php if($produtoscadast[$posicao]["nome"]){echo $produtoscadast[$posicao]["nome"];} ?>">
+                        <label for="nome">Nome</label>
+                        <input type="text" class="form-control" name="nome" id="nome" value="<?php if($dadosatuais[$posicao]["nome"]){echo $dadosatuais[$posicao]["nome"];} ?>">
                     </div>
                     <div class="col-md-6">
-                        <label for="preco-produto">Preço</label>
-                        <input type="text" pattern="([0-9]{1,3}\.)?[0-9]{1,3},[0-9]{2}$" class="form-control" name="preco-produto" id="preco-produto" value="<?php if($produtoscadast[$posicao]["preco"]){echo $produtoscadast[$posicao]["preco"];} ?>" title="O número deve ter o formato 9.999,99">
+                        <label for="preco">Preço</label>
+                        <input type="text" pattern="([0-9]{1,3}\.)?[0-9]{1,3},[0-9]{2}$" class="form-control" name="preco" id="preco" value="<?php if($dadosatuais[$posicao]["preco"]){echo $dadosatuais[$posicao]["preco"];} ?>" title="O número deve ter o formato 9.999,99">
                     </div>
                 </div>
                 <div class="form-row">
                     <div class="form-group col-md-12">
-                        <label for="descricao-produto">Descrição</label>
-                        <textarea class="form-control" id="descricao-produto" rows="4"><?php if($produtoscadast[$posicao]["descricao"]){echo $produtoscadast[$posicao]["descricao"];} ?></textarea>
+                        <label for="descricao">Descrição</label>
+                        <textarea class="form-control" id="descricao" rows="4" name="descricao"><?php if($dadosatuais[$posicao]["descricao"]){echo $dadosatuais[$posicao]["descricao"];} ?></textarea>
                     </div>
                 </div>
                 <div class="form-group">
-                    <img src="<?php if($produtoscadast[$posicao]["Enderecofoto"]){echo $produtoscadast[$posicao]["Enderecofoto"];} ?>" class="img-fluid" style="object-fit: cover; object-position: middle;width: 100%; max-height: 300px; border-radius: 3px;" alt="produto">
+                    <img src="<?php if($dadosatuais[$posicao]["Enderecofoto"]){echo $dadosatuais[$posicao]["Enderecofoto"];} ?>" class="img-fluid" style="object-fit: cover; object-position: middle;width: 100%; max-height: 300px; border-radius: 3px;" alt="produto">
                 </div>
                 <div class="form-row">
                     <div class="form-group col-md-12">
                         <div class="custom-file">
-                        <input type="file" class="custom-file-input" id="fotoProd" name="fotoProd" required>
+                        <input type="file" class="custom-file-input" id="fotoProd" name="fotoProd" >
                         <label class="custom-file-label" for="fotoProd" data-browse="Buscar">Selecione a foto</label>
                         </div>
                     </div>
