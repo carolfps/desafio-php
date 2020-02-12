29e0fed1c98dd984109820fb490542aa7e9f1efa
carolfps <37373110+carolfps@users.noreply.github.com>
2020-02-12 15:52:54 -0300

    correcao logica verificacao login usuario

--- login.php
+++ login.php
@@ -1,34 +1,48 @@
 <?php
-require('./includes/navbar.php');
+
 session_start();
+
 if (isset($_POST["entrar"])) {
 
     $usuarios = file_get_contents("usuarios.json");
     $usuarios = json_decode($usuarios, true);
+
     if (!isset($_SESSION["senhaok"])) {
+
         $_SESSION["senhaok"] = 0;
+
     }
-    if (isset($_SESSION)) {
-        foreach ($usuarios as $usuario) {
-            //a chave para buscar a senha do usuario eh o email
-            if ($usuario["email"] == $_POST["email"]) {
-                //se a senha que a pessoa inseriu for igual a senha cadastrada salva no json, a var session recebe os dados do usuario
-                if (password_verify($_POST["senha"], $usuario["senha"])) {
-                    $_SESSION["email"] = $usuario["email"];
-                    $_SESSION["senha"] = $usuario["senha"];
-                    $_SESSION["nome"] = $usuario["nome"];
-                    $_SESSION["senhaok"] = 1;
-                    header("Location: indexprodutos.php");
-                }
-            }
+
+    //descobrindo a posicao do usuario que esta logando
+    $posicao = array_search($_POST["email"], array_column($usuarios, 'email'));
+
+    //a chave para buscar a senha do usuario eh o email
+    if ($usuarios[$posicao]["email"] == $_POST["email"]) {
+
+        //se a senha que a pessoa inseriu for igual a senha cadastrada salva no json, a var session recebe os dados do usuario
+        if (password_verify($_POST["senha"], $usuarios[$posicao]["senha"])) {
+            
+            $_SESSION["email"] = $usuarios[$posicao]["email"];
+            $_SESSION["senha"] = $usuarios[$posicao]["senha"];
+            $_SESSION["nome"] = $usuarios[$posicao]["nome"];
+            $_SESSION["senhaok"] = 1;
+
+            header("Location: indexprodutos.php");
+
         }
+
     }
 }
+
 //se ja existe uma sessao aberta, redireciona para a pagina indexprodutos.php (home do sistema)
 if (isset($_SESSION["senhaok"])) {
+
     if ($_SESSION["senhaok"] == 1) {
+
         header("Location: indexprodutos.php");
+
     }
+
 }
 
 ?>
@@ -42,6 +56,7 @@ if (isset($_SESSION["senhaok"])) {
 </head>
 
 <body class="bg-light">
+    <?php require('./includes/navbar.php');?> 
     <main>
         <div class="d-flex justify-content-center">
 
@@ -57,12 +72,16 @@ if (isset($_SESSION["senhaok"])) {
                         <label for="senha">Senha</label>
                         <input type="password" class="form-control rounded-pill" id="senha" name="senha" required>
                     </div>
+
                     <?php
                     if (isset($_SESSION["senhaok"])) {
                         if ($_SESSION["senhaok"] == 0) { ?>
+
                             <small class="text-danger">Usuário ou senha incorretos!</small><br>
+
                     <?php }
                     } ?>
+                    
                     <input class="btn btn-primary mt-4 btn-block rounded-pill" type="submit" value="Entrar" name="entrar">
                 </form>
                 <small>Ainda não tem cadastro? <a href="cadastro.php" style="display: inline-block;">Clique aqui e registre-se.</a></small>
