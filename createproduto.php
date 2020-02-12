d89961b77c0e81e6ebe6d69c65693064b02ae0aa
carolfps <37373110+carolfps@users.noreply.github.com>
2020-02-12 15:29:13 -0300

    ajuste verificacao de upload de foto

--- createproduto.php
+++ createproduto.php
@@ -1,5 +1,7 @@
 <?php
+
 session_start();
+
 //se não tiver logado redireciona para a pagina de login
 if(!isset($_SESSION["email"])){
     header("Location: login.php");
@@ -13,57 +15,56 @@ if ($_POST) {
         "descricao" => $_POST["descricao-produto"]
     );
 
-    if (file_exists('cadastprodutos.json')) {
+    $dadosatuais = file_get_contents("cadastprodutos.json");
+    $temporario = json_decode($dadosatuais, true);
+
+    //se esse for o primeiro produto cadastrado no json o id dele será 1, se não será o (último id) + 1
+    if (!isset($temporario[0]["id"])) {
+
+        $produto["id"] = 1;
+
+    } else {
+
+        $maiorindice = count($temporario) - 1;
+        $proximoid = $temporario[$maiorindice]["id"] + 1;
+        $produto["id"] = $proximoid;
+
+    }
+
+    if ($_FILES) {
+        
+        //renomeando a foto para o padrao Produto1, Produto2, ...
+        $ext = strtolower(pathinfo($_FILES["fotoProd"]["name"], PATHINFO_EXTENSION));
+        $nome = "Produto".$produto["id"].".".$ext;
+        $arquivo = $_FILES["fotoProd"]["tmp_name"];
+        $caminho = "uploads\\" . $nome;
+
+        //inserindo a informacao de endereco da foto no json
+        $produto["Enderecofoto"] = $caminho;
+
+        $uploadOk = 1;
+
+        //permitindo apenas determinadas extensões de arquivo
+        if ($ext != "jpg" && $ext != "png" && $ext != "jpeg" && $ext != "gif") {
+            $uploadOk = 0;               
+        } else {
+            //se tudo estiver OK tenta subir o arquivo
+            $movendo = move_uploaded_file($arquivo, $caminho);
+        }
+
+    }
+
+    //se o upload da foto der certo, insere no json os dados do novo produto cadastrado
+    if ($uploadOk == 1) {
 
         $dadosatuais = file_get_contents("cadastprodutos.json");
         $temporario = json_decode($dadosatuais, true);
-
-        //se esse for o primeiro produto cadastrado no json o id dele será 1, se não será o (último id) + 1
-        if (!isset($temporario[0]["id"])) {
-            $produto["id"] = 1;
-        } else {
-            $maiorindice = count($temporario) - 1;
-            $proximoid = $temporario[$maiorindice]["id"] + 1;
-            $produto["id"] = $proximoid;
-        }
-
-        if ($_FILES) {
-            
-            //renomeando a foto para o padrao Produto1, Produto2, ...
-            $ext = strtolower(pathinfo($_FILES["fotoProd"]["name"], PATHINFO_EXTENSION));
-            $nome = "Produto".$produto["id"].".".$ext;
-            $arquivo = $_FILES["fotoProd"]["tmp_name"];
-            $caminho = "uploads\\" . $nome;
-
-            //inserindo a informacao de endereco da foto no json
-            $produto["Enderecofoto"] = $caminho;
-
-            $uploadOk = 1;
-
-            //permitindo apenas determinadas extensões de arquivo
-            if ($ext != "jpg" && $ext != "png" && $ext != "jpeg" && $ext != "gif") {
-                $uploadOk = 0;
-            }
-
-            //se nao for possivel fazer o upload da foto imprime uma msg na tela
-            if ($uploadOk == 0) {
-                echo "Desculpa, não foi possível subir o seu arquivo.";                
-            } else {
-                //se tudo estiver OK tenta subir o arquivo
-                $movendo = move_uploaded_file($arquivo, $caminho);
-            }
-        }
-
-        //inserindo no json os dados do novo produto cadastrado
-        if (file_exists('cadastprodutos.json') ) {
-            $dadosatuais = file_get_contents("cadastprodutos.json");
-            $temporario = json_decode($dadosatuais, true);
-            $temporario[] = $produto;
-            $jsonData = json_encode($temporario);
-            file_put_contents("cadastprodutos.json", $jsonData);
-            header("Location: indexprodutos.php");
-        }
-    }
+        $temporario[] = $produto;
+        $jsonData = json_encode($temporario);
+        file_put_contents("cadastprodutos.json", $jsonData);
+        header("Location: indexprodutos.php");
+        
+    }    
 
 }
 ?>
@@ -103,6 +104,7 @@ if ($_POST) {
                         <div class="custom-file">
                             <input type="file" class="custom-file-input" id="fotoProd" name="fotoProd">
                             <label class="custom-file-label" for="fotoProd" data-browse="Buscar">Selecione a foto</label>
+                            <?php if (isset($uploadOk) and $uploadOk == 0) { ?><small class="form-text text-danger">Desculpa, não foi possível subir o seu arquivo. Formatos aceitos: JPG, JPEG, PNG, GIF.</small><?php } ?>
                         </div>
                     </div>
                 </div>
